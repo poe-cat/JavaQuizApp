b82bffd6b8f55f5869c82160d68dadf4aa14f056
poe-cat <katarzyna.tramea@gmail.com>
2022-04-29 13:49:25 +0200

    create question form service

---
+++ src/main/java/com/poecat/JavaQuizApp/service/QuestionForm.java
@@ -0,0 +1,20 @@
+package com.poecat.JavaQuizApp.service;
+
+import com.poecat.JavaQuizApp.model.Question;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class QuestionForm {
+
+    private List<Question> questions;
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+}
