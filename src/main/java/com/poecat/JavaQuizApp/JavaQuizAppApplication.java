adbb505a2e96b985d5b17e614ce85405c3c05139
poe-cat <katarzyna.tramea@gmail.com>
2022-04-29 13:36:27 +0200

    first commit (model and repo)

---
+++ src/main/java/com/poecat/JavaQuizApp/JavaQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.poecat.JavaQuizApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaQuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poecat/JavaQuizApp/JavaQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poecat.JavaQuizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
