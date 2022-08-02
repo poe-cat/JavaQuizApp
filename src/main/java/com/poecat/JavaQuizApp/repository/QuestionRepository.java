1126d2764692165399772522fc15236886ceb2ff
poe-cat <katarzyna.tramea@gmail.com>
2022-08-02 16:52:17 +0200

    Create test for QuestionForm

--- src/main/java/com/poecat/JavaQuizApp/repository/QuestionRepository.java
+++ src/main/java/com/poecat/JavaQuizApp/repository/QuestionRepository.java
@@ -4,6 +4,10 @@ import com.poecat.JavaQuizApp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+    List<Question> getAllQuestions();
 }
--- src/main/java/com/poecat/JavaQuizApp/service/QuestionForm.java
+++ src/main/java/com/poecat/JavaQuizApp/service/QuestionForm.java
@@ -1,14 +1,25 @@
 package com.poecat.JavaQuizApp.service;
 
 import com.poecat.JavaQuizApp.model.Question;
+import com.poecat.JavaQuizApp.repository.QuestionRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class QuestionForm {
 
     private List<Question> questions;
+    private QuestionRepository questionRepository;
+
+    public QuestionForm(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+    List<Question> getAllQuestions() {
+        return new ArrayList<>(questionRepository.getAllQuestions());
+    }
 
     public List<Question> getQuestions() {
         return questions;
