21873cfb7ad2901f66ad00a75ecee21a5cee183c
poe-cat <katarzyna.tramea@gmail.com>
2022-08-03 12:44:37 +0200

    Add unit tests for testing CRUD operations for UserRepository

--- src/test/java/com/poecat/JavaQuizApp/repository/UserRepositoryTest.java
+++ src/test/java/com/poecat/JavaQuizApp/repository/UserRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.poecat.JavaQuizApp.repository;
 
 import com.poecat.JavaQuizApp.model.User;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.test.annotation.Rollback;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserRepositoryTest {
 
     @Autowired
@@ -19,9 +23,44 @@ public class UserRepositoryTest {
 
     @Test
     @Rollback(false)
-    public void createNewUser() {
+    @Order(1)
+    public void shouldCreateNewUser() {
         User user = userRepository.save(new User(1L, "elaine@luned.com", "password", "Elaine", "Luned"));
 
         assertThat(user.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    @Order(2)
+    public void shouldFindUserByFirstName() {
+        User user = userRepository.findByFirstName("Elaine");
+        assertThat(user.getFirstName()).isEqualTo("Elaine");
+    }
+
+    @Test
+    @Rollback(false)
+    @Order(3)
+    public void shouldUpdateUsersLastName() {
+        User user = userRepository.findByFirstName("Elaine");
+        user.setLastName("Fea");
+
+        userRepository.save(user);
+
+        User updatedUser = userRepository.findByFirstName("Elaine");
+
+        assertThat(updatedUser.getLastName()).isEqualTo("Fea");
+    }
+
+    @Test
+    @Rollback(false)
+    @Order(4)
+    public void shouldDeleteUser() {
+        User user = userRepository.findByFirstName("Elaine");
+
+        userRepository.deleteById(user.getId());
+
+        User deletedUser = userRepository.findByFirstName("Elaine");
+
+        assertThat(deletedUser).isNull();
     }
 }
