9f687a9fb520fe7206d721824b12ac7f5889590a
poe-cat <katarzyna.tramea@gmail.com>
2022-08-07 15:14:12 +0200

    Create test class for QuestionsController

---
+++ src/test/java/com/poecat/JavaQuizApp/controller/QuestionsControllerTest.java
@@ -0,0 +1,19 @@
+package com.poecat.JavaQuizApp.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class QuestionsControllerTest {
+
+    @Test
+    public void shouldShowNewQuestionForm() {
+
+        //given
+        QuestionsController questionsController = mock(QuestionsController.class);
+
+        //when
+
+    }
+}
