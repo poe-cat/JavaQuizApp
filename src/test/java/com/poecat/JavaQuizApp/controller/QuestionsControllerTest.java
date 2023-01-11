841f6bbbec50553a13b4f1c776e3d64e4a01c9a6
poe-cat <katarzyna.tramea@gmail.com>
2023-01-11 13:54:36 +0100

    create test for QuestionController (showing new question form)

--- src/test/java/com/poecat/JavaQuizApp/controller/QuestionsControllerTest.java
+++ src/test/java/com/poecat/JavaQuizApp/controller/QuestionsControllerTest.java
@@ -1,26 +1,39 @@
 package com.poecat.JavaQuizApp.controller;
 
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import com.poecat.JavaQuizApp.repository.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.BindingResult;
 
-@WebMvcTest(QuestionsController.class)
-public class QuestionsControllerTest {
+@ExtendWith(MockitoExtension.class)
+class QuestionsControllerTest {
 
-    @Autowired
+    @Mock
+    private QuestionRepository questionRepository;
+    @Mock
+    private BindingResult bindingResult;
+    @InjectMocks
+    private QuestionsController questionsController;
     private MockMvc mockMvc;
 
-    @MockBean
-    private QuestionRepository questionRepository;
-
-    @Test
-    public void authenticatedUserShouldBeAbleToAddQuestion() {
-
-
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(questionsController).build();
     }
 
+    @Test
+    void showNewQuestionForm() throws Exception {
+        mockMvc.perform(get("/newQuestionForm"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("new_question"))
+                .andExpect(model().attributeExists("question"));
+    }
 }
